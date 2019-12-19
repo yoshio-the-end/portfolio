25bbb8d60b582535c96580bbec6f8189bb599dc9
yoshio-the-end <shoma.yoshikawa9126@gmail.com>
2019-12-19 22:02:32 +0900

    add_main_conponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from "./components/Header";
+import Main from "./components/Main";
 import './App.scss';
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.scss';
+
+
+export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="main-container">
+        <p>メイン</p>
+      </div>
+    )
+  }
+}
