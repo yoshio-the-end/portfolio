397324dd03b499a9277e224f5e5faccf652a32cc
yoshio-the-end <shoma.yoshikawa9126@gmail.com>
2019-12-31 18:13:38 +0900

    edit_responsive

--- src/components/ContactIcons.js
+++ src/components/ContactIcons.js
@@ -9,7 +9,6 @@ export default class ContactIcons extends React.Component {
     return (
       <div class="main__article__icons">
         <a href="https://twitter.com/ZN59FOGeBBTQ0et" className="main__article__icons__icon"><i class="fab fa-twitter-square"></i></a>
-        <a href="#" className="main__article__icons__icon"><i class="fab fa-line"></i></a>
         <a href="https://www.instagram.com/xamhos/?hl=ja" className="main__article__icons__icon"><i class="fab fa-instagram"></i></a>
       </div>
     )
